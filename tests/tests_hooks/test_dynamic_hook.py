1115d9e2d751bc48f544ddafe59ce911a4e87a96
aymanelsayeed <ayman.elsayeed@mdclone.com>
2023-11-26 13:25:18 +0200

    add examples.

--- tests/tests_hooks/test_dynamic_hook.py
+++ tests/tests_hooks/test_dynamic_hook.py
@@ -10,24 +10,32 @@ from hooks.dynamic import dynamic_decorator
 
 
 @pytest.mark.hook
-@dynamic_decorator(owner="ayman")
+@dynamic_decorator(owner="ayman", tag=["Hook"])
 class TestDynamicHook:
 
-    @allure_hook(tag=["Report3"], severity="critical")
-    def test_report3_1(self):
+    @allure_hook(severity="critical")
+    def test_set_severity_critical(self):
         """
         Example on pr, how to write test for syn-engine
         :return: None
         """
         assert True
 
-    @allure_hook(tag=["Report3"], severity="blocker")
-    def test_report3_2(self):
+    @allure_hook(severity="blocker")
+    def test_test_set_severity_blocker(self):
         assert True
 
-    @allure_hook(tag=["Report3"], severity="trivial")
-    def test_report3_3(self):
+    @allure_hook(severity="trivial")
+    def test_test_set_severity_trivial(self):
         assert True
 
     def test_fixture_usage(self, customer):
         assert customer == "customer"
+
+    @allure_hook(owner="Ayman.E")
+    def test_change_owner(self):
+        assert True
+
+    @allure_hook(tags=["tag1", "tag2"])
+    def test_add_tags(self):
+        assert True
