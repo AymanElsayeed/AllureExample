d76a2e20d34988a6774010ff25ee9170a515b818
aymanelsayeed <ayman.elsayeed@mdclone.com>
2023-11-26 12:46:44 +0200

    apply reports story to all.

--- tests/test_attatch/test_attatch_html.py
+++ tests/test_attatch/test_attatch_html.py
@@ -3,6 +3,7 @@ import pytest
 import allure
 
 
+@allure.story("Reports")
 @pytest.mark.attach
 class TestAttach:
 
@@ -12,21 +13,18 @@ class TestAttach:
         assert True
 
     def test_attach_html_table(self):
-        allure.dynamic.story("Reports")
         allure.dynamic.title("Files Execution Time")
         df = pd.DataFrame({"file name": ['file one', 'file two'], "time": [3, 4]})
         allure.attach(df.to_html(index=False), name="Attach html table", attachment_type=allure.attachment_type.HTML)
         assert True
 
     def test_attach_data_frame(self):
-        allure.dynamic.story("Reports")
         allure.dynamic.title("Files info")
         df = pd.DataFrame({"file name": ['file one', 'file two'], "size": [3, 4]})
         allure.attach(df.to_csv(index=False), name="Attach data frame", attachment_type=allure.attachment_type.CSV)
         assert True
 
     def test_attach_text(self):
-        allure.dynamic.story("Reports")
         allure.dynamic.title("Attach text")
         allure.attach("Attach text", name="Attach text", attachment_type=allure.attachment_type.TEXT)
         assert True
