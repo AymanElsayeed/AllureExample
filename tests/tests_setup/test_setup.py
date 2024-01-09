beedb6548061e7134c4d7aa1d7d547e4a8cc704c
aymanelsayeed <ayman.elsayeed@mdclone.com>
2024-01-09 15:29:14 +0200

    add examples.

--- tests/tests_setup/test_setup.py
+++ tests/tests_setup/test_setup.py
@@ -1,4 +1,3 @@
-
 import pytest
 import allure
 
@@ -6,7 +5,6 @@ import allure
 @pytest.mark.xdist_group(name="setup")
 @pytest.mark.setup
 class TestSetUp:
-
     class_data = []
 
     @pytest.mark.parametrize("file_name", [2, 3, 4])
@@ -34,6 +32,21 @@ class TestSetUp:
         allure.attach(f"setup method {method.__name__}", name="setup method",
                       attachment_type=allure.attachment_type.TEXT)
 
+        if method.__name__ == "test_setup_failed_1":
+            self.valid = False
+
+        if method.__name__.startswith("test_setup_failed"):
+            # get number from the test parm
+            for mark in method.pytestmark:
+                if mark.name == "parametrize":
+
+                    if mark.args[0] != "number":
+                        self.valid = True
+
+                    for number in mark.args[1]:
+                        if number % 2 == 0:
+                            self.valid = False
+
     @classmethod
     def setup_class(cls, **kwargs):
         """
@@ -42,3 +55,26 @@ class TestSetUp:
         """
         allure.attach(f"Ayman Class setup method ", name="setup method",
                       attachment_type=allure.attachment_type.TEXT)
+
+    def test_setup_failed_1(self):
+        """
+        test setup failed
+        :return: None
+        """
+        assert self.valid is False
+
+    @pytest.mark.parametrize("number", [20])
+    def test_setup_failed_2(self, number):
+        """
+        test setup failed
+        :return: None
+        """
+        assert self.valid is False
+
+    @pytest.mark.parametrize("a_number", [21, 20])
+    def test_setup_failed_3(self, a_number):
+        """
+        test setup failed
+        :return: None
+        """
+        assert self.valid
