4463503a3b5e2569c5e0442740a3e01b921c0e71
aymanelsayeed <ayman.elsayeed@mdclone.com>
2024-07-27 15:12:59 +0300

    add docstring.

--- tests/tests_setup/test_setup.py
+++ tests/tests_setup/test_setup.py
@@ -1,3 +1,13 @@
+"""
+Test setup methods.
+
+Objectives:
+    - Demonstrate how to use setup methods.
+    - Demonstrate how to use setup_class method.
+
+
+"""
+
 import pytest
 import allure
 
@@ -9,31 +19,36 @@ class TestSetUp:
 
     @pytest.mark.parametrize("file_name", [2, 3, 4])
     def test_setup_1(self, file_name):
-        print(f"test setup {file_name}")
-        assert True
+        allure.dynamic.description("This test is to demonstrate the setup method")
+        assert self.valid
 
     @pytest.mark.parametrize("file_name", [22, 23, 24])
     def test_setup_2(self, file_name):
-        print(f"test setup {file_name}")
-        assert True
+        allure.dynamic.description("This test is to demonstrate the setup method")
+        assert self.valid
 
     @pytest.mark.parametrize("file_name", [32, 33, 34])
     def test_setup_3(self, file_name):
-        print(f"test setup {file_name}")
-        assert True
+        allure.dynamic.description("This test is to demonstrate the setup method")
+        assert self.valid
 
     def setup_method(self, method):
         """
         setup method.
         :return: None
         """
+        # append the method name to the class_data list
         self.class_data.append(method.__name__)
-        print(f"setup method {method.__name__}")
+
         allure.attach(f"setup method {method.__name__}", name="setup method",
                       attachment_type=allure.attachment_type.TEXT)
 
         if method.__name__ == "test_setup_failed_1":
             self.valid = False
+            self.reason = f"the test {method.__name__} should fail"
+        else:
+            self.valid = True
+            self.reason = "the test expected to pass"
 
         if method.__name__.startswith("test_setup_failed"):
             # get number from the test parm
@@ -42,10 +57,12 @@ class TestSetUp:
 
                     if mark.args[0] != "number":
                         self.valid = True
+                        self.reason = "the test should pass, the first parameter is not called number"
 
                     for number in mark.args[1]:
                         if number % 2 == 0:
                             self.valid = False
+                            self.reason = f"number {number} is even"
 
     @classmethod
     def setup_class(cls, **kwargs):
@@ -77,4 +94,4 @@ class TestSetUp:
         test setup failed
         :return: None
         """
-        assert self.valid
+        assert not self.valid
