203c89cc63960a023ec12ff5f017217d4f0f37e5
aymanelsayeed <ayman.elsayeed@mdclone.com>
2024-07-27 14:40:54 +0300

    add allure description.

--- tests/tests_fixtures/conftest.py
+++ tests/tests_fixtures/conftest.py
@@ -6,20 +6,25 @@ those fixtures are available for test_fixtures package only.
 """
 
 import pytest
+import allure
 
 
 @pytest.fixture(scope="function", name="list_of_numbers")
 def fixture_list_of_numbers() -> list:
+    # allure description
+    allure.dynamic.description("This fixture returns a list of numbers")
     return [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 @pytest.fixture(scope="function", name="list_of_even_numbers")
 def fixture_list_of_sub_numbers(list_of_numbers) -> list:
-    # return only the even numbers
+    # allure description
+    allure.dynamic.description("This fixture returns a list of even numbers")
     return list(filter(lambda x: x % 2 == 0, list_of_numbers))
 
 
 @pytest.fixture(scope="function", name="list_of_odd_numbers")
 def fixture_list_of_odd_numbers(list_of_numbers) -> list:
-    # return only the odd numbers
+    # allure description
+    allure.dynamic.description("This fixture returns a list of odd numbers")
     return list(filter(lambda x: x % 2 != 0, list_of_numbers))
